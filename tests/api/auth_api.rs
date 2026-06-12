d8fe746b7696fe9f75def6545b305e77c4bfc080
Brian-Magomere-Igadwa <igadwabrian7@gmail.com>
2026-06-12 10:39:45 +0300

    test(Green): Implement logic to turn mis_shaped_auth_requests_are_rejected green.

--- tests/api/auth_api.rs
+++ tests/api/auth_api.rs
@@ -1,32 +1,27 @@
+use std::collections::HashMap;
+
+use actix_web::http::StatusCode;
+use fake::{Fake, Faker};
+
 use crate::helpers::spawn_app;
 
 #[tokio::test]
-async fn requests_missing_authorization_are_rejected() {
+async fn mis_shaped_auth_requests_are_rejected() {
     // Arrange
     let app = spawn_app().await;
+    let nonsensical_mis_shaped_payload: HashMap<String, String> = Faker.fake();
 
     //Act
     let response = reqwest::Client::new()
         .post(&format!("{}/auth", &app.address))
-        .json(&serde_json::json!({
-        "title": "Test title",
-        "content": {
-            "text": "Test body as plain text",
-        "html": "<p>Test body as HTML</p>",
-        }
-        }))
+        .json(&serde_json::json!(nonsensical_mis_shaped_payload))
         .send()
         .await
         .expect("Failed to execute request.");
 
     // Assert
-    assert_eq!(401, response.status().as_u16());
-    assert_eq!(
-        r#"Basic realm="publish""#,
-        response.headers()["WWW-Authenticate"]
-    );
+    assert_eq!(StatusCode::BAD_REQUEST, response.status().as_u16());
 }
-
 //signup
 
 //signin
