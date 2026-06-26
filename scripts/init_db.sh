ec8c682a0f8bbc9b9dcaea261bf84c58dfc783ba
Brian-Magomere-Igadwa <igadwabrian7@gmail.com>
2026-06-26 08:49:21 +0300

    chore(Scripts): Update the hibp url in config files as well as the shape of the docker command in the shell script.

--- scripts/init_db.sh
+++ scripts/init_db.sh
@@ -27,9 +27,9 @@ DB_PORT="${POSTGRES_PORT:=5432}"
 if [[ -z "${SKIP_DOCKER}" ]]; then
 	docker run \
 		--name authpracticedb \
-		-e POSTGRES_USER=${DB_USER} \
-		-e POSTGRES_PASSWORD=${DB_PASSWORD} \
-		-e POSTGRES_DB=${DB_NAME} \
+		-e POSTGRES_USER="${DB_USER}" \
+		-e POSTGRES_PASSWORD="${DB_PASSWORD}" \
+		-e POSTGRES_DB="${DB_NAME}" \
 		-p "${DB_PORT}":5432 \
 		-d postgres \
 		postgres -N 1000
