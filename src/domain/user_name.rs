741f3a5a42efa765277488fff330598a7da20286
Brian-Magomere-Igadwa <igadwabrian7@gmail.com>
2026-07-02 00:22:58 +0300

    build: Get rid of the old unmaintained claim crate.

--- src/domain/user_name.rs
+++ src/domain/user_name.rs
@@ -27,45 +27,45 @@ impl AsRef<str> for UserName {
 #[cfg(test)]
 mod tests {
     use crate::domain::UserName;
-    use claim::{assert_err, assert_ok};
+
     #[test]
     fn a_256_grapheme_long_name_is_valid() {
         let name = "a".repeat(256);
-        assert_ok!(UserName::parse(name));
+        assert!(UserName::parse(name).is_ok());
     }
     #[test]
     fn a_name_longer_than_256_graphemes_is_rejected() {
         let name = "a".repeat(257);
-        assert_err!(UserName::parse(name));
+        assert!(UserName::parse(name).is_err());
     }
 
     #[test]
     fn a_name_shorter_than_256_graphemes_is_rejected() {
         let name = "a".repeat(7);
-        assert_err!(UserName::parse(name));
+        assert!(UserName::parse(name).is_err());
     }
 
     #[test]
     fn whitespace_only_names_are_rejected() {
         let name = " ".to_string();
-        assert_err!(UserName::parse(name));
+        assert!(UserName::parse(name).is_err());
     }
     #[test]
     fn empty_string_is_rejected() {
         let name = "".to_string();
-        assert_err!(UserName::parse(name));
+        assert!(UserName::parse(name).is_err());
     }
     #[test]
     fn names_containing_an_invalid_character_are_rejected() {
         for name in &['/', '(', ')', '"', '<', '>', '\\', '{', '}'] {
             let name = name.to_string();
-            assert_err!(UserName::parse(name));
+            assert!(UserName::parse(name).is_err());
         }
     }
 
     #[test]
     fn a_valid_name_is_parsed_successfully() {
         let name = "John De Reffic".to_string();
-        assert_ok!(UserName::parse(name));
+        assert!(UserName::parse(name).is_ok());
     }
 }
