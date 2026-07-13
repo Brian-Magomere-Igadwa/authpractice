1770d9502fb5aad3362d321982105f94b1dad913
Brian-Magomere-Igadwa <igadwabrian7@gmail.com>
2026-07-13 10:44:14 +0300

    refactor(App): Centralize the responsibility of configuring the app to a consumer facing build method.
    Refs: #3

--- src/startup.rs
+++ src/startup.rs
@@ -3,13 +3,54 @@ use std::net::TcpListener;
 use actix_web::{App, HttpResponse, HttpServer, Responder, dev::Server, web};
 
 use metrics_exporter_prometheus::{PrometheusBuilder, PrometheusHandle};
-use sqlx::PgPool;
+use secrecy::Secret;
+use sqlx::{PgPool, postgres::PgPoolOptions};
 use tracing_actix_web::TracingLogger;
 
 use crate::{
+    configuration::{DatabaseSettings, Settings},
     end_points::{AUTH, HEALTH_CHECK, METRICS, USERS},
     routes::{create_user_account, health_check, login},
 };
+
+pub struct Application {
+    port: u16,
+    server: Server,
+}
+
+impl Application {
+    pub async fn build(configuration: Settings) -> Result<Self, anyhow::Error> {
+        let connection_pool = get_connection_pool(&configuration.database);
+
+        let address = format!(
+            "{}:{}",
+            configuration.application.host, configuration.application.port
+        );
+        let listener = TcpListener::bind(address)?;
+        let port = listener.local_addr().unwrap().port();
+        let server = run(
+            listener,
+            connection_pool,
+            configuration.application.hibp_api_url,
+            configuration.redis_uri,
+        )?;
+        Ok(Self { port, server })
+    }
+
+    pub fn port(&self) -> u16 {
+        self.port
+    }
+
+    pub async fn run_until_stopped(self) -> Result<(), std::io::Error> {
+        self.server.await
+    }
+}
+
+pub fn get_connection_pool(configuration: &DatabaseSettings) -> PgPool {
+    PgPoolOptions::new().connect_lazy_with(
+        configuration.connect_options()
+    )
+}
 
 pub struct ApplicationBaseUrl(pub String);
 
