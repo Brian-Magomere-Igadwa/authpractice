20cb325251865bdc44e362d3a58ef060c737a5e7
Brian-Magomere-Igadwa <igadwabrian7@gmail.com>
2026-07-20 12:55:54 +0300

    refactor(LoginError): Refactor ResponseError for LoginError to include status 429 as well us less repetition of build to reduce loc.

--- src/routes/login.rs
+++ src/routes/login.rs
@@ -32,69 +32,28 @@ impl ResponseError for LoginError {
             LoginError::UnexpectedError(_) => StatusCode::INTERNAL_SERVER_ERROR,
             LoginError::AuthError(_) => StatusCode::UNAUTHORIZED,
             LoginError::ValidationError(_) => StatusCode::BAD_REQUEST,
+            LoginError::TooManyRequests => StatusCode::TOO_MANY_REQUESTS,
         }
     }
+
     fn error_response(&self) -> HttpResponse<actix_web::body::BoxBody> {
-        match self {
+        let (error_type, message) = match self {
             LoginError::UnexpectedError(_) => {
-                HttpResponse::build(self.status_code()).json(serde_json::json!({
-                    "error": "Internal server error",
-                    "message": "Something went wrong on our end."
-                }))
+                ("Internal server error", "Something went wrong on our end.")
             }
-            LoginError::AuthError(_) => {
-                HttpResponse::build(self.status_code()).json(serde_json::json!({
-                    "error": "Wrong credentials.",
-                    "message": "You are not allowed to login with that information provided."
-                }))
-            }
+            LoginError::AuthError(_) => (
+                "Wrong credentials.",
+                "You are not allowed to login with that information provided.",
+            ),
             LoginError::ValidationError(_) => {
-                HttpResponse::build(self.status_code()).json(serde_json::json!({
-                    "error": "Validation failed.",
-                    "message": "You entered invalid credentials."
-                }))
+                ("Validation failed.", "You entered invalid credentials.")
             }
-        }
-    }
-}
+            LoginError::TooManyRequests => ("Too Many Requests", "Please try again later"),
+        };
 
-pub async fn login(
-    form: web::Json<FormData>,
-    pool: web::Data<PgPool>,
-    session: TypedSession,
-    hibp_url: web::Data<ApplicationBaseUrl>,
-) -> Result<HttpResponse, LoginError> {
-    //fetch the user from db
-    // hash the passed password from form.password
-    // compare the results
-    // if they match then we create a session for them else invalid creds
-
-    let credentials = Credentials {
-        username: UserName::parse(form.0.name).map_err(LoginError::ValidationError)?,
-        password: UserPassword::parse(form.0.password, &hibp_url.0)
-            .await
-            .map_err(LoginError::ValidationError)?,
-    };
-
-    tracing::Span::current().record(
-        "username",
-        tracing::field::display(&credentials.username.as_ref()),
-    );
-    match validate_credentials(credentials, &pool).await {
-        Ok(user_id) => {
-            tracing::Span::current().record("user_id", tracing::field::display(&user_id));
-            session.renew();
-            session
-                .insert_user_id(user_id)
-                .map_err(|e| LoginError::UnexpectedError(e.into()))?;
-            Ok(HttpResponse::Ok().finish())
-        }
-        Err(e) => {
-            let e = match e {
-                AuthError::InvalidCredentials(_) => LoginError::AuthError(e.into()),
-                AuthError::UnexpectedError(_) => LoginError::UnexpectedError(e.into()),
-            };
-            Err(e)
-        }
+        HttpResponse::build(self.status_code()).json(serde_json::json!({
+            "error": error_type,
+            "message": message
+        }))
     }
 }
