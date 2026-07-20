58ae82dfc4da6bdbde7d8e87d6668959c49fe5da
Brian-Magomere-Igadwa <igadwabrian7@gmail.com>
2026-07-20 12:11:31 +0300

    fix(App): Actually monitor the application by running it watching incase it stops after calling build.
    This will fix the issue where the app could instantly start and immediately stop thereafter.

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,11 @@
+use std::fmt::{Debug, Display};
+
 use authpractice::{
     configuration::get_configuration,
     startup::{Application, init_metrics_recorder},
     telemetry::{get_subscriber, init_subscriber},
 };
+use tokio::task::JoinError;
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
@@ -15,7 +18,34 @@ async fn main() -> anyhow::Result<()> {
     // Bubble up the io::Error if we failed to bind the address
     let configuration = get_configuration().expect("Failed to read configuration.");
 
-    Application::build(configuration.clone()).await?;
+    let application = Application::build(configuration.clone()).await?;
+    let application_task = tokio::spawn(application.run_until_stopped());
+    let outcome = application_task.await;
+    report_exit("API", outcome);
 
     Ok(())
+}
+
+fn report_exit(task_name: &str, outcome: Result<Result<(), impl Debug + Display>, JoinError>) {
+    match outcome {
+        Ok(Ok(())) => {
+            tracing::info!("{} has exited", task_name)
+        }
+        Ok(Err(e)) => {
+            tracing::error!(
+                error.cause_chain = ?e,
+                error.message = %e,
+                "{} failed",
+                task_name
+            )
+        }
+        Err(e) => {
+            tracing::error!(
+                error.cause_chain = ?e,
+                error.message = %e,
+                "{}' task failed to complete",
+                task_name
+            )
+        }
+    }
 }
